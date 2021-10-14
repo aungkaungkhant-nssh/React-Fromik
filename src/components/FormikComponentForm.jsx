45beaa891f7dcda0571bb243cbf96b67efb18755
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-10-14 10:36:04 +0630

    load save data

--- src/components/FormikComponentForm.jsx
+++ src/components/FormikComponentForm.jsx
@@ -1,133 +1,162 @@
 import { Formik,Form ,Field, ErrorMessage, FastField, FieldArray} from 'formik'
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from 'yup'
-function FormikComponentForm() {
-    const initialValues={
-        name:"",
-        email:"",
-        comments:"",
-        address:"",
-        socialMedia:{
-           
-        },
-        phone:["",""],
-        phoneNumbers:[""],
+const initialValues={
+    name:"",
+    email:"",
+    comments:"",
+    address:"",
+    socialMedia:{
+       
+    },
+    phone:["",""],
+    phoneNumbers:[""],
 
-    }
-    const validationSchema=Yup.object({
-        name:Yup.string().required("Required"),
-        email:Yup.string().email("Invalid format").required("Required"),
-        comments:Yup.string().required("Required"),
-        address:Yup.string().required("Required"),
-        socialMedia:Yup.object().required("Required"),
-        phone:Yup.array().required("Required")
-    })
-    const onSubmit=values=>console.log(values)
+}
+const validationSchema=Yup.object({
+    name:Yup.string().required("Required"),
+    email:Yup.string().email("Invalid format").required("Required"),
+    comments:Yup.string().required("Required"),
+    address:Yup.string().required("Required"),
+    socialMedia:Yup.object().required("Required"),
+    phone:Yup.array().required("Required")
+})
+const loadsaveData={
+    name:"Aung Kaung Khant",
+    email:"akkgit0909@gmail.com",
+    comments:"testing",
+    address:"Pyin Oo Lwin",
+    social:{
+        facebook:"",
+        twitter:""
+    },
+    phone:["",""],
+    phoneNumbers:[""]
+}
+const onSubmit=values=>console.log(values)
+function FormikComponentForm() {
+    
+    const [saveFormValues,setSaveFormValues]=useState(null);
     return (
+        
         <div className="container">
             <Formik 
-                initialValues={initialValues}
+                initialValues={ saveFormValues||initialValues}
                 validationSchema={validationSchema}
+                enableReinitialize //loadsavedata
                 onSubmit={onSubmit}
+                // validateOnChange={false}
+                // validateOnBlur={false}
+                
             >
-                <Form>
-                    <div className="form-group">
-                        <label htmlFor="">Name</label>
-                        <Field name="name" type="text" className="form-control"></Field>
-                        <ErrorMessage name="name">
-                            {
-                                errorMessage=><div className="text-danger">{errorMessage}</div>
-                            }
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Email</label>
-                        <Field name="email" type="email" className="form-control"></Field>
-                        <ErrorMessage name="email">
-                            {
-                                errorMessage=><div className="text-danger">{errorMessage}</div>
-                            }
-                        </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                         <label htmlFor="">Comments</label>
-                         <Field as="textarea" name="comments" className="form-control"></Field>
-                         <ErrorMessage name="comments">
-                             {
-                                 errorMessage=><div className="text-danger">{errorMessage}</div>
-                             }
-                         </ErrorMessage>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Address</label>
-                        <FastField name="address">
-                            {
-                                props=>{
-                                    console.log("first field")
-                                    const {field,form,meta}=props
-                                    return (
-                                        <div>
-                                            <input type="text" {...field} className="form-control"/>
-                                            {
-                                                meta.touched && meta.error ? <div className="text-danger">
-                                                    {meta.error}
-                                                </div>:null
-                                            }
-                                        </div>   
-                                    )
-                                }
-                            }
-                        </FastField>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Facebook</label>
-                        <Field name="socialMedia.facebook" type="text" className="form-control"></Field>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Twitter</label>
-                        <Field name="socialMedia.twitter" type="text" className="form-control"></Field>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Phone 1</label>
-                        <Field name="phone[0]" type="text" className="form-control"></Field>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Phone 2</label>
-                        <Field name="phone[1]" type="text" className="form-control"></Field>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="">Phone Numbers One</label>
-                        <FieldArray name="phoneNumbers">
-                            {
-                                props=>{
-                                   const {form,push,remove}=props;
-                                    const {values}=form;
-                                    const {phoneNumbers}=values;
-                                    return(
-                                        <div>
-                                            {
-                                                phoneNumbers.map((phoneNumber,index) =>{
-                                                    return(
-                                                        <div key={index}>
-                                                            <Field name={`phoneNumbers[${index}]`}></Field>
-                                                            {
-                                                                index>0 &&
-                                                                <button onClick={()=>remove(index)}>-</button>
-                                                            }
-                                                            
-                                                            <button onClick={()=>push("")}>+</button>
-                                                        </div>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    )
-                                }
-                            }
-                        </FieldArray>
-                    </div>
-                    <input type="submit" className="btn btn-success btn-block"/>
-                </Form>
+                {
+                    formik=>{
+                        console.log(formik)
+                        return(
+                            <Form>
+                            <div className="form-group">
+                                <label htmlFor="">Name</label>
+                                <Field name="name" type="text" className="form-control"></Field>
+                                <ErrorMessage name="name">
+                                    {
+                                        errorMessage=><div className="text-danger">{errorMessage}</div>
+                                    }
+                                </ErrorMessage>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Email</label>
+                                <Field name="email" type="email" className="form-control"></Field>
+                                <ErrorMessage name="email">
+                                    {
+                                        errorMessage=><div className="text-danger">{errorMessage}</div>
+                                    }
+                                </ErrorMessage>
+                            </div>
+                            <div className="form-group">
+                                 <label htmlFor="">Comments</label>
+                                 <Field as="textarea" name="comments" className="form-control"></Field>
+                                 <ErrorMessage name="comments">
+                                     {
+                                         errorMessage=><div className="text-danger">{errorMessage}</div>
+                                     }
+                                 </ErrorMessage>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Address</label>
+                                <FastField name="address">
+                                    {
+                                        props=>{
+                                            console.log("first field")
+                                            const {field,form,meta}=props
+                                            return (
+                                                <div>
+                                                    <input type="text" {...field} className="form-control"/>
+                                                    {
+                                                        meta.touched && meta.error ? <div className="text-danger">
+                                                            {meta.error}
+                                                        </div>:null
+                                                    }
+                                                </div>   
+                                            )
+                                        }
+                                    }
+                                </FastField>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Facebook</label>
+                                <Field name="socialMedia.facebook" type="text" className="form-control"></Field>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Twitter</label>
+                                <Field name="socialMedia.twitter" type="text" className="form-control"></Field>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Phone 1</label>
+                                <Field name="phone[0]" type="text" className="form-control"></Field>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Phone 2</label>
+                                <Field name="phone[1]" type="text" className="form-control"></Field>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="">Phone Numbers One</label>
+                                <FieldArray name="phoneNumbers">
+                                    {
+                                        props=>{
+                                           const {form,push,remove}=props;
+                                            const {values}=form;
+                                            const {phoneNumbers}=values;
+                                            return(
+                                                <div>
+                                                    {
+                                                        phoneNumbers.map((phoneNumber,index) =>{
+                                                            return(
+                                                                <div key={index}>
+                                                                    <Field name={`phoneNumbers[${index}]`}></Field>
+                                                                    {
+                                                                        index>0 &&
+                                                                        <button onClick={()=>remove(index)}>-</button>
+                                                                    }
+                                                                    
+                                                                    <button onClick={()=>push("")}>+</button>
+                                                                </div>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            )
+                                        }
+                                    }
+                                </FieldArray>
+                            </div>
+                            <button onClick={()=>setSaveFormValues(loadsaveData)}className="btn btn-warning mb-2">Load Save Data</button>
+                            <input type="submit" className="btn btn-success btn-block" disabled={!formik.isValid && !formik.isSubmitting}/>
+                          
+                        </Form>
+                        )
+                    }
+                }
+               
              </Formik>
         </div>
       
